27babaf5182518d0224f138ec50732d88c2ac640
Rawle Juglal <rawledjuglal@gmail.com>
2023-05-19 17:38:28 -0500

    creating layout route
    Building structure for routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom'
+import { Layout, loader as layoutLoader} from './pages/Layout/Layout'
 import './index.css'
+
+const router = createBrowserRouter(createRoutesFromElements(
+  <Route path='/' element={<Layout />} loader={layoutLoader}>
+
+  </Route>
+))
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
