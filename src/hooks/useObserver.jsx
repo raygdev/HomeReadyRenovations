2e1a3d607446c6610755fc48a65ce3402a0d53e5
Bence Fekete <fekete965@gmail.com>
2023-05-25 11:35:46 +0100

    Ray - Create a useObserver Hook

---
+++ src/hooks/useObserver.jsx
@@ -0,0 +1,28 @@
+import { useState, useEffect } from "react";
+
+export default function useObserver(node) {
+  const [isIntersecting, setIsIntersecting] = useState(false);
+
+  useEffect(() => {
+    let options = {
+      rootMargin: "10px",
+      threshold: 0.5,
+    };
+    const observer = new IntersectionObserver(handleObserver, options);
+
+    if (node.current) {
+      observer.observe(node.current);
+    }
+
+    return () => {
+      observer.disconnect();
+    };
+  }, [isIntersecting]);
+
+  function handleObserver(entries) {
+    const [entry] = entries;
+    setIsIntersecting(entry.isIntersecting);
+  }
+
+  return isIntersecting;
+}
