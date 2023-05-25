504172d26e0b32f8bd024f479aa8aec8cf52b4db
Rawle Juglal <RawleJuglal@users.noreply.github.com>
2023-05-25 15:12:06 -0500

    Revert "Main - switch values based on intersection"

--- src/components/Jumbo/Jumbo.jsx
+++ src/components/Jumbo/Jumbo.jsx
@@ -1,20 +1,11 @@
-import React, { useRef, useEffect, useContext } from 'react'
+import React from 'react'
 import Lottie from "lottie-react";
 import Home3D from '../../assets/home3d.json'
-import useObserver from '../../hooks/useObserver';
-import { Context } from "../../context/SideBarContext"
 import './Jumbo.css'
 
 const Jumbo = ()=>{
-    const homeRef = useRef(null)
-    const isIntersecting = useObserver(homeRef)
-    const { setSideBarValue } = useContext(Context)
-
-    useEffect(() => {
-        isIntersecting ? setSideBarValue("Home") : null
-    },[isIntersecting])
     return(
-        <section ref={homeRef} id="Home" className='--layout-jumbo-section'>
+        <section id="Home" className='--layout-jumbo-section'>
             <Lottie id='jumbo-anim' className='--layout-jumbo-anim'  animationData={Home3D} loop={true} />
             <h2 className='--layout-jumbo-title'>Quality Renovations for LARGE & small Homes</h2>
             <p className='--layout-jumbo-text'>Revamp your living space with our exceptional home renovation services. We deliver functional and stylish results that increase your property value. Let's bring your vision to life!</p>
