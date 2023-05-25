4463a70a1fa2b9c55d9fac36ba2cf1002cb246f6
Bence Fekete <fekete965@gmail.com>
2023-05-25 11:31:12 +0100

    Bence - Split up routes and run prettier on main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,35 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom'
-import { Layout, loader as layoutLoader} from './pages/Layout/Layout'
-import Error from './components/Error/Error'
-import Home from './pages/Home/Home'
-import Portfolio from './pages/Portfolio/Portfolio'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
+import { Layout, loader as layoutLoader } from "./pages/Layout/Layout";
+import Error from "./components/Error/Error";
+import Home from "./pages/Home/Home";
+import Portfolio from "./pages/Portfolio/Portfolio";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+import SideBarContext from "./context/SideBarContext";
 
-const router = createBrowserRouter(createRoutesFromElements(
-  <Route path='/' element={<Layout />} loader={layoutLoader} errorElement={<Error />}>
-    <Route path='portfolio' element={<Portfolio/>} />
-    <Route index element={<Home />}/>
+const routes = createRoutesFromElements(
+  <Route
+    path="/"
+    element={<Layout />}
+    loader={layoutLoader}
+    errorElement={<Error />}
+  >
+    <Route path="portfolio" element={<Portfolio />} />
+    <Route index element={<Home />} />
   </Route>
-  
-  
-))
+);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter(routes);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
